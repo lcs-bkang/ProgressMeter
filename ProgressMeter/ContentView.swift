5eab8c3755b29e1b3e1b86cd1ead44337875927d
Bradley Kang <bkang@lcs.on.ca>
2021-02-18 09:43:23 -0500

    First iteration

--- ProgressMeter/ContentView.swift
+++ ProgressMeter/ContentView.swift
@@ -8,9 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // For driving animation to reveal rectangle with progress meter fill
+    @State private var progressMeterOffset = CGSize.zero
+    
+    // Width of the meter
+    let meterWidth: CGFloat = 100
+
+    // Thickness of meter's border
+    let borderWidth: CGFloat = 2
+    
+    // Padding at top of progress meter
+    let verticalPadding: CGFloat = 44
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            ZStack {
+                // "Fill" for progress meter; stationary
+                Rectangle()
+                    .frame(width: meterWidth, height: 548 - verticalPadding, alignment: .center)
+
+                // Will slide up
+                Rectangle()
+                    .fill(Color.primary)
+                    .colorInvert()
+                    .frame(width: meterWidth, height: 548 - verticalPadding, alignment: .center)
+                    .offset(progressMeterOffset)
+                    .onAppear(perform: {
+                        withAnimation(Animation.easeIn(duration: 4.0)) {
+                            // Offset is moves the opaque rectangle up
+                            progressMeterOffset = CGSize(width: 0, height: -1 * (548 - verticalPadding))
+                        }
+                    })
+
+                // Sits above the rectangle that slides up (in the z-axis)
+                // This means the rectangle sliding up will pass beneath this view
+                Rectangle()
+                    .fill(Color(hue: 0, saturation: 0, brightness: 0, opacity: 0))
+                    .frame(width: meterWidth + borderWidth, height: 548 - verticalPadding + borderWidth, alignment: .center)
+                    .overlay(
+                        Rectangle()
+                            .stroke(Color.primary, lineWidth: borderWidth)
+                    )
+
+            }
+
+        }
+
     }
 }
 
@@ -19,3 +63,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
